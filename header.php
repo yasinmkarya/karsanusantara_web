7e750d5ed8ded8c367d7a82cae0f04cb5801cedf
yasinmkarya <yasin.success@gmail.com>
2017-04-07 00:28:17 +0700

    menambahkan responsive comment dan pagination

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
     <div class="blog-masthead">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item active" href= "<?php bloginfo('template_directory'); ?>index.php"> Home </a>
           <?php wp_list_pages('&title_li='); ?>
         </nav>
       </div>
